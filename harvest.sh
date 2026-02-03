b17096e65a853dcd58e030dabf3abed02e3284a6
Eliot Jordan <eliot.jordan@gmail.com>
2026-02-03 12:15:39 -0600

    Upload parquet and embeddings to R2

--- harvest.sh
+++ harvest.sh
@@ -3,3 +3,7 @@
 mkdir tmp
 uv run ogm-harvest --download
 duckdb -c ".read convert_geometry.sql"
+mkdir tmp/data/
+mv tmp/cloud.parquet tmp/data/.
+mv tmp/ogm-model/embeddings.bin tmp/data/.
+mv tmp/ogm-model/tokenizer.json tmp/data/.
