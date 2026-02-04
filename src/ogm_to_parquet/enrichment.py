68ebb6895f6735505c0d9c80f5b352c1583e7b0b
Eliot Jordan <eliot.jordan@gmail.com>
2026-02-04 17:29:59 -0600

    Add IIIF

--- src/ogm_to_parquet/enrichment.py
+++ src/ogm_to_parquet/enrichment.py
@@ -24,6 +24,8 @@ EXCLUDED_SERVICE_KEYS = {
     "http://www.opengis.net/def/serviceType/ogc/wms",
     "http://www.opengis.net/def/serviceType/ogc/wmts",
     "https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames",
+    "http://iiif.io/api/presentation#manifest",
+    "http://iiif.io/api/image"
 }
 
 # Formats eligible for cloud derivative generation
